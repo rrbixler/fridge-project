67d086e1a0fb45c93b9cceed0c28f5227a51af2f
rrbixler <rrbixler@sandiego.edu>
2019-04-25 10:43:41 -0400

    Config.js file
    connected the front end to the back end using the config file
    added heroku app url to production url location

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://tranquil-plains-95557.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
